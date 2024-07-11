cae3afb95882a7a290726f54b86c59f5bc6fee59
Silvi Sinanaj <silvi.sinanaj@blacklane.com>
2024-07-11 21:57:11 +0200

    fix z-index button

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,15 +5,15 @@ import Background from '@/components/background'
 export default function NotFound() {
   return (
     <div className='flex flex-col items-center justify-center h-screen'>
-      <Background />
       <h2 className='text-3xl font-bold mb-4'>Not Found</h2>
       <p className='text-lg mb-8'>Could not find the requested resource.</p>
       <Link
-        className='bg-primary text-white rounded-lg px-4 py-2  hover:bg-primary/80'
+        className='bg-primary text-white rounded-lg px-4 py-2 z-10 hover:bg-primary/80'
         href='/'
       >
         Return Home
       </Link>
+      <Background />
     </div>
   )
 }
