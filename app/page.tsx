e6bf1489c097bdb05fd7fd279dc0c79fcd445fa4
Silvi Sinanaj <silvi.sinanaj@blacklane.com>
2024-07-11 20:42:11 +0200

    experimenting with mega

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,77 @@
-import { Link } from '@nextui-org/link'
-import { Snippet } from '@nextui-org/snippet'
-import { Code } from '@nextui-org/code'
-import { button as buttonStyles } from '@nextui-org/theme'
+'use client'
+import { useEffect } from 'react'
+import { Storage } from 'megajs'
 
-import { siteConfig } from '@/config/site'
-import { GithubIcon } from '@/components/icons'
-import { Counter } from '@/components/counter'
 import { Hero } from '@/components/hero'
+// import Timeline from '@/components/timeline'
 
 export default function Home() {
+  useEffect(() => {
+    startMega().catch((error) => {
+      // Handle the error
+      console.error('Error during MEGA initialization:', error)
+    })
+  }, [])
+
   return (
-    <section className='flex flex-col items-center justify-center gap-4 py-8 md:py-10'>
+    <section className='flex flex-col items-start justify-center gap-4 pb-8'>
       <Hero />
-      <div className='flex gap-3'>
-        <Link
-          isExternal
-          className={buttonStyles({
-            color: 'primary',
-            radius: 'full',
-            variant: 'shadow',
-          })}
-          href={siteConfig.links.linkedin}
-        >
-          LinkedIn
-        </Link>
-        <Link
-          isExternal
-          className={buttonStyles({ variant: 'bordered', radius: 'full' })}
-          href={siteConfig.links.github}
-        >
-          <GithubIcon size={20} />
-          GitHub
-        </Link>
-        <Counter />
-      </div>
-      <div className='mt-8'>
-        <Snippet hideCopyButton hideSymbol variant='bordered'>
-          <span>
-            Get started by editing <Code color='primary'>app/page.tsx</Code>
-          </span>
-        </Snippet>
-      </div>
+      {/* <Timeline /> */}
     </section>
   )
+}
+
+const startMega = async () => {
+  const userAgent =
+    typeof window !== 'undefined' && window.navigator
+      ? window.navigator.userAgent
+      : 'Mozilla/5.0'
+
+  const megaPassword = process.env.NEXT_PUBLIC_MEGA_PASSWORD
+
+  if (!megaPassword) {
+    console.error('MEGA_PASSWORD environment variable is not defined')
+
+    return
+  }
+  console.log('megaPassword', megaPassword)
+
+  if (!userAgent) {
+    console.error('User agent is not defined')
+  }
+
+  const userAgentComponents = userAgent.split(' ')
+
+  console.log(
+    'User agent:',
+    userAgentComponents.find((c) => c.includes('Chrome')) ||
+      userAgentComponents.find((c) => c.includes('Mozilla')) ||
+      userAgentComponents[userAgentComponents.length - 1]
+  )
+
+  try {
+    const storage = new Storage({
+      email: 'silviberat@gmail.com',
+      password: megaPassword,
+      userAgent: userAgent,
+    })
+
+    // Will resolve once the user is logged in
+    // or reject if some error happens
+    await storage.ready
+    console.log('Storage ready:', storage)
+
+    const res = await storage.login()
+
+    console.log('Login successful:', res)
+
+    // Example of creating a folder
+    // const folderRes = await storage.createFolder({ name: 'test' });
+    // console.log('Folder created:', folderRes);
+  } catch (error) {
+    console.error('Failed to initialize MEGA storage:', error)
+    console.log(error, 'here')
+
+    throw error
+  }
 }
